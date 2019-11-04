70549fc4f10e549c928b0a5c94db08d4e201f200
eduardopiste <eduardoenigma12@gmail.com>
2019-11-03 23:11:31 -0600

    creamos la pagina

---
+++ docs/include/core/app/view/index-view.php
@@ -0,0 +1,9 @@
+<section class="content">
+<div class="row">
+<div class="col-md-12">
+    
+<h1>Bienvenido al Sistema</h1>
+<p>SystemAssistance es un sistema para tomar asistencia por personas por dia.</p>
+</div>
+</div>
+</section>
